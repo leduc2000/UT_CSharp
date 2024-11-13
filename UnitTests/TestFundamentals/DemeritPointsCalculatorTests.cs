8548d4fb3e8ca8b725112419dedd17082604cfc2
LVN Le Van Duc <levanduc@luvina.net>
2024-11-13 17:36:27 +0700

    Commit day 3

---
+++ UnitTests/TestFundamentals/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace UnitTests.TestFundamentals
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        [Test]
+        [TestCase(0, 0)]
+        public void CalculateDemeritPoints_Case1() {
+
+        }
+    }
+}
---
+++ UnitTests/TestFundamentals/FizzBuzzTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace UnitTests.TestFundamentals
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_Case1_ReturnsFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(30);
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutput_Case2_ReturnsFizz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_Case3_ReturnsBuzz()
+        {
+            var result = FizzBuzz.GetOutput(5);
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+    }
+}
