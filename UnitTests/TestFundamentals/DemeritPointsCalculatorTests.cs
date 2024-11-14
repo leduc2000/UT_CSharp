7476b84687b19989e149b6a6233e347729d228c8
LVN Le Van Duc <levanduc@luvina.net>
2024-11-14 18:05:31 +0700

    commit

--- UnitTests/TestFundamentals/DemeritPointsCalculatorTests.cs
+++ UnitTests/TestFundamentals/DemeritPointsCalculatorTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
 
 namespace UnitTests.TestFundamentals
 {
@@ -6,9 +8,30 @@ namespace UnitTests.TestFundamentals
     public class DemeritPointsCalculatorTests
     {
         [Test]
-        [TestCase(0, 0)]
-        public void CalculateDemeritPoints_Case1() {
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_SpeedIsOutOfRange_ThrowArgumentOutOfRangeException(int speed)
+        {
+            var calculator = new DemeritPointsCalculator();
 
+            Assert.That(() => calculator.CalculateDemeritPoints(speed),
+                Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+        
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        public void CalculateDemeritPoints_WhenCalled_ReturnDemeritPoints(int speed, int expectedResult)
+        {
+            var calculator = new DemeritPointsCalculator();
+
+            var points = calculator.CalculateDemeritPoints(speed);
+            
+            Assert.That(points, Is.EqualTo(expectedResult));
         }
     }
 }
